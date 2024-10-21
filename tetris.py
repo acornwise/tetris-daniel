9a014da47411efaa56de00de1ee1261202255695
dgoh1 <dgoh0611@gmail.com>
2024-10-21 18:54:54 +1100

    Update tetris.py

--- tetris.py
+++ tetris.py
@@ -1,17 +1,10 @@
 # import pygame
 # def main():
- 
 #     pygame.init()
-    
-
 #     width, height = 500, 600
 #     screen = pygame.display.set_mode((width, height))
-    
-
 #     pygame.display.set_caption("Pygame Window")
-
 #     running = True
-
 #     while running:
 #         for event in pygame.event.get():
 #             if event.type == pygame.QUIT:
@@ -20,9 +13,6 @@
 #                 pygame.quit()
 # if __name__ == "__main__":
 #     main()
-
-
-
 # S = [['.....',
 #       '......',
 #       '..00..',
@@ -33,7 +23,6 @@
 #       '..00.',
 #       '...0.',
 #       '.....']]
-
 # Z = [['.....',
 #       '.....',
 #       '.00..',
@@ -44,7 +33,6 @@
 #       '.00..',
 #       '.0...',
 #       '.....']]
-
 # I = [['..0..',
 #       '..0..',
 #       '..0..',
@@ -55,13 +43,11 @@
 #       '.....',
 #       '.....',
 #       '.....']]
-
 # O = [['.....',
 #       '.....',
 #       '.00..',
 #       '.00..',
 #       '.....']]
-
 # J = [['.....',
 #       '.0...',
 #       '.000.',
@@ -82,7 +68,6 @@
 #       '..0..',
 #       '.00..',
 #       '.....']]
-
 # L = [['.....',
 #       '...0.',
 #       '.000.',
@@ -103,7 +88,6 @@
 #       '..0..',
 #       '..0..',
 #       '.....']]
-
 # T = [['.....',
 #       '..0..',
 #       '.000.',
@@ -124,53 +108,35 @@
 #       '.00..',
 #       '..0..',
 #       '.....']]
-
 # shapes = [S, Z, I, O, J, L, T]
 # shape_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 165, 0), (0, 0, 255), (128, 0, 128)]
-
 # class Piece(object):
 #     pass
-
 # def create_grid(locked_positions={}):
 #     pass
-
 # def convert_shape_format(shape):
 #     pass
-
 # def valid_space(shape, grid):
 #     pass
-
 # def check_lost(positions):
 #     pass
-
 # def get_shape():
 #     pass
-
 # def draw_text_middle(text, size, color, surface):  
-#     pass
-   
+#     pass  
 # def draw_grid(surface, row, col):
 #     pass
-
 # def clear_rows(grid, locked):
 #     pass
-
 # def draw_next_shape(shape, surface):
 #     pass
-
 # def draw_window(surface):
 #     pass
-
 # def main():
 #     pass
-
 # def main_menu():
 #     pass
-
 # main_menu()  # start game
-
-
-
 # class Piece(object):
 #     rows = 20  
 #     columns = 10 
@@ -180,29 +146,17 @@
 #         self.shape = shape
 #         self.color = shape_colors[shapes.index(shape)]
 #         self.rotation = 0
-
-
-
 # def create_grid(locked_positions={}):
 #     grid = [[(0,0,0) for x in range(10)] for x in range(20)]
- 
 #     for i in range(len(grid)):
 #         for j in range(len(grid[i])):
 #             if (j,i) in locked_positions:
 #                 c = locked_positions[(j,i)]
 #                 grid[i][j] = c
 #     return grid
-
-
-
 # def get_shape():
 #     global shapes, shape_colors
- 
 #     return Piece(5, 0, random.choice(shapes))
-
-
-
-
 #  surface.fill((0,0,0))
 
 #     font = pygame.font.SysFont(\'comicsans', 60)
